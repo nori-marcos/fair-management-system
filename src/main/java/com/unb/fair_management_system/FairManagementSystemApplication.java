3600f00a2df5810b17dbb58eeac1cb5372cacd3e
nori <marcos.noriyuki.miyata@gmail.com>
2025-06-24 20:44:39 -0300

    Initial commit

---
+++ src/main/java/com/unb/fair_management_system/FairManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.unb.fair_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairManagementSystemApplication.class, args);
+	}
+
+}
