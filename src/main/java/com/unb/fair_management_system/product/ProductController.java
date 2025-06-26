0fe58ca1df0387eda953fef2b3b3dd65b81deefe
nori <marcos.noriyuki.miyata@gmail.com>
2025-06-26 13:05:01 -0300

    Add createProduct endpoint to ProductController

--- src/main/java/com/unb/fair_management_system/product/ProductController.java
+++ src/main/java/com/unb/fair_management_system/product/ProductController.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +19,18 @@ import java.util.List;
 public class ProductController {
   private final ProductRepository productRepository;
 
-  @Operation(summary = "List")
+  @Operation(summary = "List all products")
   @GetApiResponses
   @GetMapping
   public List<Product> getAllProducts() {
     return productRepository.findAll();
   }
+
+  @Operation(summary = "Create a new product")
+  @GetApiResponses
+  @PostMapping
+  public Product createProduct(@RequestBody Product product) {
+    return productRepository.save(product);
+  }
+
 }
